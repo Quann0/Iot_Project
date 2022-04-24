83617b3f539dba5c52fd98f00fa63085ca520cd0
Quan <mas0987169143@gmail.com>
2022-04-24 21:38:48 +0700

    Login_Save-Account-to_file.json

---
+++ UART_CS/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace UART_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AccountBox = new System.Windows.Forms.TextBox();
+            this.PassWordBox = new System.Windows.Forms.TextBox();
+            this.Account = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.SignUp = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AccountBox
+            // 
+            this.AccountBox.Location = new System.Drawing.Point(288, 122);
+            this.AccountBox.Name = "AccountBox";
+            this.AccountBox.Size = new System.Drawing.Size(125, 27);
+            this.AccountBox.TabIndex = 0;
+            this.AccountBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AccountBox_KeyDown);
+            // 
+            // PassWordBox
+            // 
+            this.PassWordBox.Location = new System.Drawing.Point(288, 169);
+            this.PassWordBox.Name = "PassWordBox";
+            this.PassWordBox.Size = new System.Drawing.Size(125, 27);
+            this.PassWordBox.TabIndex = 1;
+            this.PassWordBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PassWordBox_KeyDown);
+            // 
+            // Account
+            // 
+            this.Account.AutoSize = true;
+            this.Account.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Account.Location = new System.Drawing.Point(191, 123);
+            this.Account.Name = "Account";
+            this.Account.Size = new System.Drawing.Size(73, 23);
+            this.Account.TabIndex = 2;
+            this.Account.Text = "Account";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(174, 176);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PassWord";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(288, 225);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(94, 29);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // SignUp
+            // 
+            this.SignUp.AutoSize = true;
+            this.SignUp.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SignUp.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.SignUp.Location = new System.Drawing.Point(403, 227);
+            this.SignUp.Name = "SignUp";
+            this.SignUp.Size = new System.Drawing.Size(65, 23);
+            this.SignUp.TabIndex = 5;
+            this.SignUp.Text = "SignUp";
+            this.SignUp.Click += new System.EventHandler(this.SignUp_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SignUp);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Account);
+            this.Controls.Add(this.PassWordBox);
+            this.Controls.Add(this.AccountBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AccountBox;
+        private System.Windows.Forms.TextBox PassWordBox;
+        private System.Windows.Forms.Label Account;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Label SignUp;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UART_CS/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using System.Xml.Serialization;
+//using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+//using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace UART_CS
+{
+    public class Account
+    {
+        public string tk { set; get; }
+        public string mk { set; get; }
+        public Account(string _tk, string _mk)
+        {
+            tk = _tk;
+            mk = _mk;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile(@"C:\Users\QUAN\source\repos\UART_CS\UART_CS\login.json");
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+            
+
+            if (AccountBox.Text == configurationRoot.GetSection("tk").Value && PassWordBox.Text == configurationRoot.GetSection("mk").Value)
+                MessageBox.Show("Đăng nhập thành công");
+            else MessageBox.Show("Nhập sai tài khoản hoặc mật khẩu","Loi",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+        }
+
+        private void SignUp_Click(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            Button button = new Button();
+            List<Label> label = new List<Label> { new Label { },new Label {},new Label() };
+            
+            f.Controls.Add(button);
+            foreach (var item in label)
+            {
+                f.Controls.Add(item);
+            }
+        }
+            
+            private void button1_Click(object sender, EventArgs e)
+            {
+            Form f = new Form();
+            Button button = new Button() { Location = new Point(45, 181),Text = "OK" };
+            List<Label> label = new List<Label> 
+            { 
+                new Label { Location = new Point(45, 21),Text = "TaiKhoan" },
+                new Label { Location = new Point(45, 51), Text = "matkhau" },
+                new Label {Location = new Point(45, 81), Text = "Nhaplaimk" } 
+            };
+            List<TextBox> textBoxes = new List<TextBox> 
+            {
+                new TextBox{Location = new Point(120, 21)},
+                new TextBox{Location = new Point(120, 51)},
+                new TextBox{Location = new Point(120, 81)}
+            };
+            f.Controls.AddRange(textBoxes.ToArray());
+            f.Controls.Add(button);
+            foreach (var item in label)
+            {
+                f.Controls.Add(item);
+            }
+            void button_Click(object sender, EventArgs e)
+            {
+                if (textBoxes[1].Text == textBoxes[2].Text)
+                {
+                    Account account = new Account(textBoxes[0].Text.ToString(), textBoxes[1].Text.ToString());
+                    MessageBox.Show(account.tk);
+                    string json = JsonConvert.SerializeObject(account);
+                    var s = @"C:\Users\QUAN\source\repos\UART_CS\UART_CS\login.json";
+                    System.IO.File.AppendAllText(s, json);
+                    MessageBox.Show("Tao tai khoan thanh cong");
+                    f.Close();
+                }
+                else MessageBox.Show("Vui long nhap lai mat khau","Loi", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                
+            }
+            button.Click += button_Click;
+            
+            
+                f.ShowDialog();
+            }
+
+        private void PassWordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Login_Click(sender,e);
+            }
+        }
+
+        private void AccountBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Login_Click(sender, e);
+            }
+        }
+    }
+    }
